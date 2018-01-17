72504a3cc37e8315db5dd3c0601d04f400092a23
Guilherme Diego <guidiego.expgames@gmail.com>
2018-01-17 16:16:05 -0200

    Fix: Watch for CSS and Images task

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,8 +1,8 @@
 module.exports = config => {
     gulp.task('watch', ['build'], () => {
         gulp.watch(config.hbs.watch || config.hbs.input, ['handlebars']);
-        // gulp.watch(config.sass.watch || config.sass.input, ['sass']);
+        gulp.watch(config.css.watch || config.css.input, ['sass']);
         gulp.watch(config.scripts.watch || config.scripts.input, ['scripts']);
-        // gulp.watch(config.images.watch || config.images.input, ['images']);
+        gulp.watch(config.img.watch || config.img.input, ['images']);
     });
 }
